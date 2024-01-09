b7f186900b0d1c62470dc25caf6411aae4262158
Pinochino <tranhunghp22112004@gmail.com>
2024-01-09 11:15:17 +0700

    project

---
+++ src/public/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'components/head.php' ?>
+    <title>Document</title>
+</head>
+<body>
+    <?php require_once 'components/header.php' ?>
+    <main></main>
+    <?php require_once 'components/footer.php' ?>
+    <?php require_once 'components/footer.php' ?>
+
+
+
+
+
+    <?php require_once 'components/script.php' ?>
+</body>
+</html>
