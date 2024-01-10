cfcdc2e7e62ae9b5d8a779a4f567f8161e57d10d
Pinochino <tranhunghp22112004@gmail.com>
2024-01-10 11:55:57 +0700

    project

--- src/public/components/head.php
+++ src/public/components/head.php
@@ -1,1 +1,7 @@
-<header></header>
+<title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/src/public/resource/uikit-3.17.11/css/uikit-rtl.min.css">
+<link rel="stylesheet" href="/src/public/resource/style.css">
+<script src="/src/public/resource/uikit-3.17.11/js/uikit.min.js"></script>
+<script src="/src/public/resource/uikit-3.17.11/js/uikit-icons.min.js"></script>
--- src/public/components/header.php
+++ src/public/components/header.php
@@ -1,1 +1,73 @@
-<header></header>
+<header class="header">
+    <div class="header-upper">
+        <div class="uk-container uk-container-1520">
+            <div class="uk-flex uk-flex-between">
+                <div class="header-contact">
+                    <a class="header-contact-item contact-link" href=""><span uk-icon="icon: location; ratio: 1"></span><span>Find Store</span>
+                    </a>
+                    <a class="header-contact-item contact-link"><span uk-icon="icon: mail"></span><span>Tranhung@gmail.com</span></a>
+                </div>
+                <div class="header-menu">
+                    <a class="header-menu-item contact-link">Contact us</a>
+                    <a class="header-menu-item contact-link">Track your order</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header-middle">
+        <div class="uk-container uk-container-1520">
+            <div class="uk-flex uk-flex-between">
+                <div class="logo">
+                    <a href="#"><span><img src="/src/public/resource/Images/LOGO.webp" alt=""></span></a>
+                </div>
+                <div class="header-search">
+                    <form action="" class="uk-form">
+                        <div class="form-row">
+                            <input 
+                            type="text" 
+                            class="input-text" 
+                            value="" 
+                            placeholder="Search Product..." 
+                            autocompleted="off">
+                        </div>
+                        <div class="search-dropdown">
+                            <div class="search-category-title">
+                                <span>All category</span>
+                                <span class="uk-icon" uk-icon="chevron-down"></span>
+                            </div>
+                            <button class="btn-search">
+                                <span class="uk-icon" uk-icon="search"></span>
+                            </button>
+                        </div>
+                    </form>
+                    <div class="most-search">
+                        <span class="search-item">Most search: </span>
+                        <a href="" class="search-item">Iphone</a>
+                        <a href="" class="search-item">Flycam</a>
+                        <a href="" class="search-item">Xiaomi</a>
+                        <a href="" class="search-item">Nokia</a>
+                        <a href="" class="search-item">Samsung</a>
+                        <a href="" class="search-item">Dell</a>
+
+                    </div>
+                </div>
+                <div class="header-widget">
+                    <div class="uk-flex">
+                        <a href="" class="widget-item">
+                            <span class="uk-icon" uk-icon="user"></span>
+                            <span class="number">0</span>
+                        </a>
+                        <a href="" class="widget-item">
+                            <span class="uk-icon" uk-icon="heart"></span>
+                            <span class="number">3</span>
+                        </a>
+                        <a href="" class="widget-item">
+                            <span class="uk-icon" uk-icon="cart"></span>
+                            <span class="number">4</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="header"></div>
+</header>
