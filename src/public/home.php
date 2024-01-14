bf8d498cabd4fbaa3dca7b2795d5501f902eacba
Pinochino <tranhunghp22112004@gmail.com>
2024-01-14 16:25:24 +0700

    project

--- src/public/home.php
+++ src/public/home.php
@@ -9,8 +9,8 @@
     <?php require_once 'components/header.php' ?>
     <main id="homepage">
         <div class="panel-banner">
-            <div class="uk container uk-container-1520">
-                <div class="swiper uk-grid">
+            <div class="uk-container uk-container-1520">
+                <div class="uk-grid">
                     <div class="uk-width-2-3@m twoPercent">
                         <div class="panel-slide">
                             <div class="swiper-container">
@@ -20,16 +20,16 @@
                                     <div class="swiper-slide">
                                         <div class="slide-item">
                                             <span class="img img-scaledown"><img src="/src/public/resource/Images/Swiper/Swiper1.webp" alt=""></span>
-                                            <div class="slide-overlay">
-                                                <div class="slide-title">
+                                            <div class="content-overlay">
+                                                <div class="overlay-title">
                                                     Heavy on <br>
                                                     Features <br>
                                                     Light on price. <br>
                                                 </div>
-                                                <div class="slide-discription">
+                                                <div class="overlay-discription">
                                                     Start from 399
                                                 </div>
-                                                <div class="slide-readmore">
+                                                <div class="overlay-readmore">
                                                     <a href="" class="btn-readmore btn"><span>Learn more</span><span class="uk-icon" uk-icon="icon:  chevron-right"></span></a>
                                                 </div>
                                             </div>
@@ -59,10 +59,37 @@
                             </div>
                         </div>
                     </div>
-                    <div class="uk-width-1-3@m">
+                    <div class="uk-width-1-3@m onePercent">
                     <div class="banner-wrapper">
-                        <div class="banner-item"><img class="img img-cover" src="/src/public/resource/Images/Swiper/Swiper2.webp" alt=""></div>
-                        <div class="banner-item"><img class="img img-cover"  class="img img-cover"src="/src/public/resource/Images/Swiper/Swiper3.jpg" alt=""></div>
+                        <div class="banner-item"><a href=""><img class="img img-cover" src="/src/public/resource/Images/Swiper/Swiper2.webp" alt=""></a>
+                        <div class="content-overlay">
+                                                <div class="overlay-title">
+                                                    Heavy on <br>
+                                                    Features <br>
+                                                    Light on price. <br>
+                                                </div>
+                                                <div class="overlay-discription">
+                                                    Start from 399
+                                                </div>
+                                                <div class="overlay-readmore">
+                                                    <a href="" class="btn-readmore btn"><span style="padding-right: 13px;">Shop now </span><span class="uk-icon" uk-icon="icon:  chevron-right"></span></a>
+                                                </div>
+                                            </div>
+                    </div>
+                        <div class="banner-item"><a href=""><img class="img img-cover"  class="img img-cover"src="/src/public/resource/Images/Swiper/Swiper3.jpg" alt=""></a>
+                        <div class="content-overlay">
+                                                <div class="overlay-title">
+                                                    Heavy on <br>
+                                                    Features <br>
+                                                    Light on price. <br>
+                                                </div>
+                                                <div class="overlay-discription">
+                                                    Start from 399
+                                                </div>
+                                                <div class="overlay-readmore">
+                                                    <a href="" class="btn-readmore btn"><span style="padding-right: 13px;">Shop now </span><span class="uk-icon" uk-icon="icon:  chevron-right"></span></a>
+                                                </div>
+                                            </div>
                     </div>
                     </div>
 
