44d9e328e6cf7410b066f8778e43c1a9a8b16882
Pinochino <tranhunghp22112004@gmail.com>
2024-01-16 22:32:23 +0700

    project

--- src/public/components/head.php
+++ src/public/components/head.php
@@ -3,13 +3,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/src/public/resource/uikit-3.17.11/css/uikit-rtl.min.css">
 <link rel="stylesheet" href="/src/public/resource/JAVASCRIPT/node_modules/swiper/swiper-bundle.min.css">
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
-<link rel="stylesheet" href="/src/public/resource/CSS/core.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
 <link rel="stylesheet" href="/src/public/resource/CSS/style.css">
+<link rel="stylesheet" href="/src/public/resource/CSS/core.css">
 <link rel="stylesheet" href="/src/public/resource/CSS/responsive.css">
+<script src="https://kit.fontawesome.com/YOUR_KIT_CODE.js" crossorigin="anonymous"></script>
+
+
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="/src/public/resource/JAVASCRIPT/base.js"></script>
 <script src="/src/public/resource/JAVASCRIPT/jquery-3.7.1.min.js"></script>
--- src/public/home.php
+++ src/public/home.php
@@ -14,8 +14,8 @@
                     <div class="uk-width-2-3@m twoPercent">
                         <div class="panel-slide">
                             <div class="swiper-container">
-                                    <div class="swiper-button-next"></div>
-                                    <div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
                                 <div class="swiper-wrapper">
                                     <div class="swiper-slide">
                                         <div class="slide-item">
@@ -69,7 +69,7 @@
                                         Light on price. <br>
                                     </div>
                                     <div class="overlay-discription">
-                                        Start from 399
+                                        <span>Start from 399</span>
                                     </div>
                                     <div class="overlay-readmore">
                                         <a href="" class="btn-readmore btn"><span style="padding-right: 13px;">Shop now </span><span class="uk-icon" uk-icon="icon:  chevron-right"></span></a>
@@ -84,7 +84,7 @@
                                         Light on price. <br>
                                     </div>
                                     <div class="overlay-discription">
-                                        Start from 399
+                                        <span>Start from 399</span>
                                     </div>
                                     <div class="overlay-readmore">
                                         <a href="" class="btn-readmore btn"><span style="padding-right: 13px;">Shop now </span><span class="uk-icon" uk-icon="icon:  chevron-right"></span></a>
@@ -92,6 +92,91 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="panel-commit">
+            <div class="uk-container uk-container-1520">
+                <div class="panel-body">
+                    <div class="uk-grid-medium" uk-grid>
+                        <div class="uk-width-1-4@m uk-width-1-2@s">
+                            <div class="commit-item">
+                                <div class="uk-flex uk-flex-middle">
+                                    <span class="elementor-icon elementor-animation-"> <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" height="40" viewBox="0 0 512 512" width="40">
+                                            <g>
+                                                <path d="m512 234.836-99.35-127.169h-56.316v-42.667h-356.334v328.667h60.409c4.666 30.165 30.806 53.333 62.258 53.333s57.592-23.168 62.258-53.333h142.151c4.665 30.165 30.806 53.333 62.257 53.333s57.592-23.168 62.258-53.333h60.409zm-89.667 149.164c0 18.196-14.804 33-33 33s-33-14.804-33-33 14.804-33 33-33 33 14.804 33 33zm-266.666 0c0 18.196-14.804 33-33 33s-33-14.804-33-33 14.804-33 33-33 33 14.804 33 33zm-33-63c-27.624 0-51.135 17.879-59.614 42.667h-33.053v-98h205.314l29.905-104.667h-235.219v-66h296.333v268.667h-144.052c-8.479-24.788-31.99-42.667-59.614-42.667zm-92.666-85.333v-44.667h195.447l-12.762 44.667zm359.332 85.333c-12.093 0-23.396 3.432-33 9.363v-75.363h125.667v108.667h-33.053c-8.478-24.788-31.99-42.667-59.614-42.667zm-33-96v-87.333h41.684l68.23 87.333z"></path>
+                                            </g>
+                                        </svg> </span>
+                                    <div class="infor-title">
+                                        <h3 class="title"><span>FREE DELIVERY</span></h3>
+                                        <div class="description">When ordering from $500.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-4@m uk-width-1-2@s">
+                            <div class="commit-item">
+                                <div class="uk-flex uk-flex-middle">
+                                    <span class="elementor-icon elementor-animation-"> <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" height="40" viewBox="0 0 512 512" width="40">
+                                            <g>
+                                                <path d="m53.122 355.811c-15.119-30.664-23.122-64.62-23.122-99.811s8.003-69.147 23.122-99.811l-26.055-15.043c-17.702 35.2-27.067 74.307-27.067 114.854s9.365 79.654 27.067 114.853z"></path>
+                                                <path d="m458.878 156.188c15.119 30.664 23.122 64.621 23.122 99.812s-8.003 69.147-23.122 99.811l26.055 15.042c17.703-35.199 27.067-74.306 27.067-114.853s-9.365-79.654-27.068-114.854z"></path>
+                                                <path d="m68.156 130.227c8.135-12.096 17.5-23.495 28.038-34.034 39.131-39.129 90.086-62.129 144.806-65.691v-30.055c-62.739 3.601-121.22 29.735-166.019 74.533-12.418 12.418-23.392 25.895-32.862 40.215z"></path>
+                                                <path d="m241 481.498c-54.72-3.563-105.675-26.561-144.806-65.692-10.538-10.538-19.903-21.938-28.038-34.034l-26.037 15.032c9.469 14.32 20.443 27.797 32.862 40.215 44.799 44.799 103.28 70.933 166.019 74.534z"></path>
+                                                <path d="m443.844 381.772c-8.135 12.096-17.5 23.495-28.038 34.034-39.131 39.13-90.086 62.129-144.806 65.692v30.055c62.739-3.602 121.221-29.735 166.02-74.534 12.418-12.418 23.392-25.895 32.862-40.215z"></path>
+                                                <path d="m271 30.502c54.721 3.562 105.676 26.562 144.806 65.692 10.538 10.539 19.903 21.938 28.038 34.034l26.037-15.032c-9.469-14.32-20.443-27.796-32.862-40.215-44.798-44.799-103.28-70.933-166.019-74.534z"></path>
+                                                <path d="m122.65 122.65v266.7h266.7v-266.7zm236.7 236.7h-206.7v-206.7h57.867v118.13h90.965v-118.13h57.867v206.7zm-87.868-206.7v88.13h-30.965v-88.13z"></path>
+                                                <path d="m270.532 301.43h63.239v30h-63.239z"></path>
+                                            </g>
+                                        </svg> </span>
+                                    <div class="infor-title">
+                                        <h3 class="title"><span>90 DAYS RETURN</span></h3>
+                                        <div class="description">If goods have problems</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-4@m uk-width-1-2@s">
+                            <div class="commit-item">
+                                <div class="uk-flex uk-flex-middle">
+                                    <span class="elementor-icon elementor-animation-"> <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" height="40" viewBox="0 0 512 512" width="40">
+                                            <g>
+                                                <path d="m72.668 359.417h132v30h-132z"></path>
+                                                <g>
+                                                    <path d="m230.707 42.083v92.833h-163.314c-37.161.001-67.393 30.232-67.393 67.393v200.216c0 37.16 30.232 67.392 67.393 67.392h307.607c37.16 0 67.393-30.232 67.393-67.392l.005-63.444c41.538-17.802 69.602-58.99 69.602-104.183v-192.815zm-200.707 160.226c0-20.618 16.774-37.392 37.393-37.392h163.314v46.5h-200.707zm0 39.108h200.913c.85 14.814 4.571 29.037 10.743 42h-211.656zm382.393 161.108c0 20.618-16.774 37.392-37.393 37.392h-307.607c-20.619 0-37.393-16.774-37.393-37.392v-89.108h232.497c10.21 10.532 22.557 19.167 36.589 25.181l72.268 30.972 41.039-17.588zm69.607-167.627c0 33.176-19.703 63.057-50.196 76.125l-60.45 25.907-60.449-25.907c-30.494-13.068-50.197-42.949-50.197-76.125v-162.814h221.292z"></path>
+                                                </g>
+                                                <path d="m292.68 234.899c0 20.367 12.097 38.712 30.818 46.737l47.855 20.509 47.856-20.509c18.722-8.023 30.818-26.369 30.818-46.737v-130.843h-157.347zm127.347-100.843v100.843c0 8.351-4.96 15.873-12.637 19.163l-36.037 15.445-36.037-15.445c-7.677-3.29-12.637-10.812-12.637-19.163v-100.843z"></path>
+                                            </g>
+                                        </svg> </span>
+                                    <div class="infor-title">
+                                        <h3 class="title"><span>SECURE PAYMENT</span></h3>
+                                        <div class="description">100% secure payment</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-4@m uk-width-1-2@s">
+                            <div class="commit-item">
+                                <div class="uk-flex uk-flex-middle">
+                                    <span class="elementor-icon elementor-animation-"> <svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" height="40" viewBox="0 0 512 512" width="40">
+                                            <g>
+                                                <path d="m256 0c-115.008 0-208.573 93.565-208.573 208.573v109.926c0 35.39 28.435 64.245 63.655 64.966 5.296 21.153 24.459 36.869 47.23 36.869h48.688v-192.769h-48.688c-22.759 0-41.915 15.699-47.222 36.835h-33.663v-55.828c0-98.465 80.107-178.572 178.573-178.572s178.573 80.107 178.573 178.573v55.827h-33.663c-5.308-21.136-24.463-36.835-47.222-36.835h-48.688v192.768h48.688c22.771 0 41.935-15.716 47.23-36.869 12.371-.253 23.898-3.975 33.655-10.237v18.671c0 49.682-40.419 90.101-90.102 90.101h-163.138v30h163.139c66.225 0 120.102-53.877 120.102-120.101v-183.325c-.001-115.008-93.566-208.573-208.574-208.573zm-79 257.565v132.768h-18.688c-10.305 0-18.689-8.383-18.689-18.688v-95.392c0-10.305 8.384-18.688 18.689-18.688zm-99.573 60.934v-24.099h32.197v58.987c-17.993-1.432-32.197-16.533-32.197-34.888zm357.146 0c0 18.355-14.204 33.456-32.197 34.889v-58.988h32.197zm-99.573 71.834v-132.768h18.688c10.305 0 18.689 8.383 18.689 18.688v95.392c0 10.305-8.384 18.688-18.689 18.688z"></path>
+                                            </g>
+                                        </svg> </span>
+                                    <div class="infor-title">
+                                        <h3 class="title"><span>24/7 SUPPORT</span></h3>
+                                        <div class="description">Dedicated support</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
                     </div>
                 </div>
             </div>
